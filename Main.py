4c63a5392dd38b20595607391281d977a6db1e6c
Maxkoretsky <44788703+Maxkoretsky@users.noreply.github.com>
2018-11-30 12:18:10 -0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,47 @@
+# This file was created by Maxim Koretsky
+# Credits : 
+# https://docs.python-guide.org/scenarios/scrape/
+# https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/
+# https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
+# https://www.geeksforgeeks.org/working-with-pdf-files-in-python/
+# link to how to json file
+# https://medium.com/@rqaiserr/how-to-connect-to-google-sheets-with-python-83d0b96eeea6
+# link to search
+# https://code.tutsplus.com/tutorials/sorting-and-searching-in-python--cms-25668
+
+# importing the data from spreadsheet
+import datareseller
+
+# Search engine to find product
+# def linearSearch(item,my_list):
+#     found = False
+#     position = 0
+#     while position < len(my_list) and not found:
+#         if my_list[position] == item:
+#             found = True  
+#         position = position + 1
+#     return found
+
+# Data = [DataReseller]
+# item = input('What item do you want to you wish to find?')
+# itemFound = linearSearch(item,DataReseller)
+# if itemFound:
+#     print('Yes, the item is in the bag')
+# else:
+#     print('Oops, your item seems not to be in the bag')
+
+# userinput = input("choose a number")
+# selector = userinput in [3,5,2,4,1]
+# if selector == True:
+#     print("item found")
+# elif selector == false:
+#     print("Item not found")
+
+# userinput = input("choose a number")
+# if userinput in datareseller:
+#     print("Item found")
+# elif userinput in datareseller:
+#     print("Item not found")
+
+print(datareseller)
+
